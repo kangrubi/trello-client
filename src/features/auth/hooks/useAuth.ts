72117edf98e3e70e48e54c42cd57fd4053dd0798
kangrubi <rubi0624@nate.com>
2023-09-17 15:05:15 +0900

    :art: Add: uerAuth register 추가

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,1 +1,22 @@
-export const useAuth = () => {};
+import { UserResponse } from "..";
+import { storage } from "../../../utils/storage";
+import {
+  RegisterRequestDTO,
+  registerWithEmailAndPassword,
+} from "../api/register";
+
+export const useAuth = () => {
+  const handleUserResponse = (request: UserResponse) => {
+    const { user, access_token } = request;
+    storage.setToken(access_token);
+    return user;
+  };
+
+  const register = async (request: RegisterRequestDTO) => {
+    const response = await registerWithEmailAndPassword(request);
+    const user = await handleUserResponse(response);
+    return user;
+  };
+
+  return { register };
+};
