cd34207a169cb44db55144d7ecf404109e8683f6
Ruby <rkdfnql22@gmail.com>
2023-09-19 18:37:40 +0900

    :art: Add: 로그인 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "./features/auth/hooks/useAuth";
+import { useEffect } from "react";
+import { getProfile } from "./features/user/api/profile";
 
 function App() {
-  const { isLogin } = useAuth();
+  const { isLogin, signIn, signOut } = useAuth();
+  const navigate = useNavigate();
 
-  console.log(isLogin);
+  useEffect(() => {
+    if (isLogin) return;
+
+    (async () => {
+      try {
+        await getProfile();
+        signIn();
+      } catch (error) {
+        signOut();
+        navigate("/login");
+      }
+    })();
+  }, []);
 
   return (
     <>
