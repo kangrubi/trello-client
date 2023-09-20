e0c6da72a9705ee87d017b5aecd0d04c8f25d960
Ruby <rkdfnql22@gmail.com>
2023-09-20 16:51:16 +0900

    :art: Add: interceptors 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,22 @@
 import axios from "axios";
 import { API_URL } from "../config";
+import { storage } from "@/utils/storage";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = storage.getToken();
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token.accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
