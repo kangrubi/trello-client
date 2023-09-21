b891e04d6ef253e457b52c576321545f04fa5ccc
Ruby <rkdfnql22@gmail.com>
2023-09-21 14:55:10 +0900

    :label: Add: response 타입 추가

--- src/features/auth/api/refresh.ts
+++ src/features/auth/api/refresh.ts
@@ -1,5 +1,12 @@
 import { axiosInstance } from "@/lib/axios";
 
+interface PostRefreshResponse {
+  accessToken: string;
+  newRefreshToken: string;
+}
+
 export const postRefresh = async (refreshToken: string) => {
-  return axiosInstance.post("/auth/refresh", { refreshToken });
+  return axiosInstance.post<PostRefreshResponse>("/auth/refresh", {
+    refreshToken,
+  });
 };
