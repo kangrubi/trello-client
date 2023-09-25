83ba9e3052dc3eb7319d1bc0215776588c6664a4
Ruby <rkdfnql22@gmail.com>
2023-09-25 18:34:25 +0900

    :art: Update: register 기능

--- src/features/auth/components/Register.tsx
+++ src/features/auth/components/Register.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { postRegister } from "../api/register";
+
+type RegisterData = {
+  username: string;
+  email: string;
+  password: string;
+};
 
 const Register = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterData>();
+
+  const onSubmit: SubmitHandler<RegisterData> = async (data: RegisterData) => {
+    await postRegister(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="name">이름</label>
+      <input
+        type="text"
+        placeholder="name"
+        {...register("username", {
+          required: true,
+          pattern: {
+            value: /^[a-zA-Z0-9_-]+$/,
+            message:
+              "사용자 아이디에는 문자, 숫자, 하이픈, 밑줄만 포함할 수 있습니다",
+          },
+        })}
+      />
+      <p>{errors.username?.message}</p>
+      <label htmlFor="email">이메일</label>
+      <input
+        type="text"
+        placeholder="email"
+        {...register("email", {
+          required: true,
+          pattern: {
+            value:
+              /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+            message: "이메일 형식이 맞지않습니다.",
+          },
+        })}
+      />
+      <p>{errors.email?.message}</p>
+      <label htmlFor="password">패스워드</label>
+      <input
+        type="password"
+        placeholder="password"
+        {...register("password", {
+          required: true,
+          pattern: {
+            value:
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,128}$/,
+            message:
+              "패스워드 대소문자, 숫자, 특수문자 중 5가지 이상 조합하여 8~128자리 이내를 입력하세요",
+          },
+        })}
+      />
+      <p>{errors.password?.message}</p>
+      <button type="submit">회원가입</button>
+    </form>
+  );
 };
 
 export default Register;
