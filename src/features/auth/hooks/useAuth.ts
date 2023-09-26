dfff4667cff8788bfec77cfb86d038321f9e9d49
Ruby <rkdfnql22@gmail.com>
2023-09-26 17:19:55 +0900

    :art: Add: login 추가

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,10 +1,16 @@
 import { AxiosError } from "axios";
 import { postRegister } from "../api/register";
 import { CustomError } from "../types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { postLogin } from "../api/login";
 
 interface RegisterData {
   username: string;
+  email: string;
+  password: string;
+}
+
+interface LoginData {
   email: string;
   password: string;
 }
@@ -22,13 +28,25 @@ const useAuth = () => {
 
       if (!_error.response) return;
 
-      console.log("hello");
+      setError(_error.response.data);
+    }
+  };
+
+  const fetchLogin = async (data: LoginData) => {
+    try {
+      const response = await postLogin(data);
+
+      return response;
+    } catch (error: unknown) {
+      const _error = error as AxiosError<CustomError>;
+
+      if (!_error.response) return;
 
       setError(_error.response.data);
     }
   };
 
-  return { error, fetchRegister };
+  return { error, fetchRegister, fetchLogin };
 };
 
 export default useAuth;
