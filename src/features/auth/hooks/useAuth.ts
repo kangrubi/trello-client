8f728a92bf73b5f469f5f15a2f4a6f9301c1c7d2
Ruby <rkdfnql22@gmail.com>
2023-09-26 14:34:36 +0900

    :art: Add: useAuth 추가

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,7 +1,34 @@
-import React, { useState } from "react";
+import { AxiosError } from "axios";
+import { postRegister } from "../api/register";
+import { CustomError } from "../types";
+import { useEffect, useState } from "react";
+
+interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const useAuth = () => {
-  const [register, setRegister] = useState();
+  const [error, setError] = useState<CustomError>();
+
+  const fetchRegister = async (data: RegisterData) => {
+    try {
+      const response = await postRegister(data);
+
+      return response;
+    } catch (error: unknown) {
+      const _error = error as AxiosError<CustomError>;
+
+      if (!_error.response) return;
+
+      console.log("hello");
+
+      setError(_error.response.data);
+    }
+  };
+
+  return { error, fetchRegister };
 };
 
 export default useAuth;
