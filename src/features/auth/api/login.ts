58fe95399fef2a86ca00449b5d4a83c25f290007
Ruby <rkdfnql22@gmail.com>
2023-09-26 17:18:56 +0900

    :art: Add: login api 추가

---
+++ src/features/auth/api/login.ts
@@ -0,0 +1,18 @@
+import { axiosInstance } from "../../../lib/axios";
+import { PublicApiResponse } from "../types";
+
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  accessToken: string;
+}
+
+export const postLogin = (params: LoginParams) => {
+  return axiosInstance.post<PublicApiResponse<LoginResponse>>(
+    "/api/v1/auth/login",
+    params
+  );
+};
