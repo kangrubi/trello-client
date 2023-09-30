a2f12e456f1d1c54d5852ddd2eae2af9336e111c
kangrubi <rubi0624@nate.com>
2023-09-30 22:07:26 +0900

    :package: Add: mkcert 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [mkcert()],
+  server: {
+    https: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
