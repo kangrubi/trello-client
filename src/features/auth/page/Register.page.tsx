1e69a034368defb49f0a5501deea6275f20fc833
munawiki <munawiki@gmail.com>
2023-10-02 11:38:03 +0900

    🔧 fix(Register.page.tsx): add useEffect hook to register user on component mount 🔧 fix(auth.provider.tsx): refactor AuthProvider to use dependency injection for authService and add register function to AuthContext

--- src/features/auth/page/Register.page.tsx
+++ src/features/auth/page/Register.page.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useAuth from "../hooks/useAuth";
 
 const Register = () => {
+  const { authService } = useAuth();
+
+  useEffect(() => {
+    authService.register({});
+  }, []);
+
   return <div>Register.page</div>;
 };
 
--- src/features/auth/provider/auth.provider.tsx
+++ src/features/auth/provider/auth.provider.tsx
@@ -1,21 +1,40 @@
 import { createContext, useContext } from "react";
 import { AuthService } from "../service/auth.service";
 import apiService from "../../../lib/api";
+import { IRegisterRequest, IRegisterResponse } from "../types/auth.type";
 
 interface IAuthContext {
+  register: (request: IRegisterRequest) => Promise<IRegisterResponse>;
+}
+
+interface IAuthDIContainer {
   authService: AuthService;
 }
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <AuthContext.Provider
-      value={{
-        authService: new AuthService(apiService),
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+const AuthDIContainer = (
+  { authService }: IAuthDIContainer = {
+    authService: new AuthService(apiService),
+  }
+) => {
+  const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const register = async (request: IRegisterRequest) => {
+      return await authService.register(request);
+    };
+
+    return (
+      <AuthContext.Provider
+        value={{
+          register,
+        }}
+      >
+        {children}
+      </AuthContext.Provider>
+    );
+  };
+
+  return { AuthProvider };
 };
+
+export default AuthDIContainer();
