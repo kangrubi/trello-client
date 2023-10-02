921fddfe49dc909de94e85744e3dde8d597c06a4
munawiki <munawiki@gmail.com>
2023-10-02 10:00:10 +0900

    🔀 refactor(Board.layout.tsx): update BoardLayout component to render Outlet component from react-router-dom for nested routing support

--- src/features/board/layout/Board.layout.tsx
+++ src/features/board/layout/Board.layout.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const BoardLayout = () => {
-  return <div>board.layout</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default BoardLayout;
