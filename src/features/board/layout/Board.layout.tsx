5d0b1d6f56ef70d289b770dbd1e2afe280175523
munawiki <munawiki@gmail.com>
2023-10-02 09:57:14 +0900

    ✨ feat(Board.layout.tsx): add BoardLayout component to represent the layout of the board page ✨ feat(Board.page.tsx): add Board component to represent the board page

---
+++ src/features/board/layout/Board.layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BoardLayout = () => {
+  return <div>board.layout</div>;
+};
+
+export default BoardLayout;
---
+++ src/features/board/page/Board.page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Board = () => {
+  return <div>Board.page</div>;
+};
+
+export default Board;
