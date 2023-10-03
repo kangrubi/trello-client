37025603ba917d5412e71131189fef3eba2824bb
munawiki <munawiki@gmail.com>
2023-10-03 20:32:25 +0900

    🔀 refactor(Board.layout.tsx): add redirect to /board/list route on initial render to improve user experience

--- src/board/layout/Board.layout.tsx
+++ src/board/layout/Board.layout.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const BoardLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/board/list");
+  }, []);
+
   return (
     <div>
       <Outlet />
