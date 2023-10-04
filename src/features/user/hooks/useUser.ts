9ed422c262013f7f062e6128ae4b7434a78a5519
Ruby <rkdfnql22@gmail.com>
2023-10-04 18:48:48 +0900

    :art: Add: useUser 추가

---
+++ src/features/user/hooks/useUser.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { getProfile } from "../api/profile";
+import { useUserStore } from "../stores/userStore";
+import { CustomError } from "../../auth/types";
+import { AxiosError } from "axios";
+
+export const useUser = () => {
+  const [error, setError] = useState<CustomError>();
+
+  const profile = useUserStore((state) => state.profile);
+  const setProfile = useUserStore((state) => state.setProfile);
+
+  const fetchProfile = async () => {
+    try {
+      const response = await getProfile();
+
+      setProfile(response.data.data);
+    } catch (error: unknown) {
+      const _error = error as AxiosError<CustomError>;
+      if (!_error.response) return;
+      setError(_error.response.data);
+    }
+  };
+
+  return { error, profile, fetchProfile };
+};
