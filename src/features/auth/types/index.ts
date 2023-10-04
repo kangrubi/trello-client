cab9349f7b2dd1aae9d289d12a92441cec6d0c83
Ruby <rkdfnql22@gmail.com>
2023-10-04 18:47:22 +0900

    :art: Add: auth type 추가

---
+++ src/features/auth/types/index.ts
@@ -0,0 +1,27 @@
+export interface PublicApiResponse<T> {
+  data: T;
+  statusCode: number;
+  timestamp: string;
+}
+
+export interface CustomError {
+  statusCode: number;
+  message: {
+    message: string;
+    error: string;
+    statusCode: number;
+  };
+  timestamp: string;
+  path: string;
+}
+
+export interface RegisterRequest {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
