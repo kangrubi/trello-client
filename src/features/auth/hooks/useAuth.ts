4a5439515a03f64604a41e3a23700ff0809f652d
Ruby <rkdfnql22@gmail.com>
2023-10-04 17:14:58 +0900

    :art: Add: useAuth 추가

---
+++ src/features/auth/hooks/useAuth.ts
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { postRegister } from "../api/register";
+import { CustomError, LoginRequest, RegisterRequest } from "../types";
+import { AxiosError } from "axios";
+import { postLogin } from "../api/login";
+import { useAuthStore } from "../stores/authStore";
+
+const useAuth = () => {
+  const [error, setError] = useState<CustomError>();
+  const isLogin = useAuthStore((state) => state.isLogin);
+  const signIn = useAuthStore((state) => state.signIn);
+  const signOut = useAuthStore((state) => state.signOut);
+
+  const fetchRegister = async (request: RegisterRequest) => {
+    try {
+      const response = await postRegister(request);
+
+      return response;
+    } catch (error: unknown) {
+      const _error = error as AxiosError<CustomError>;
+
+      if (!_error.response) return;
+
+      setError(_error.response.data);
+    }
+  };
+
+  const fetchLogin = async (request: LoginRequest) => {
+    try {
+      const response = await postLogin(request);
+
+      signIn();
+
+      return response;
+    } catch (error: unknown) {
+      const _error = error as AxiosError<CustomError>;
+
+      if (!_error.response) return;
+
+      setError(_error.response.data);
+    }
+  };
+
+  return { error, isLogin, signIn, signOut, fetchRegister, fetchLogin };
+};
+
+export default useAuth;
