b75cee11274996040371aadaf245c702dd2e40de
Ruby <rkdfnql22@gmail.com>
2023-10-04 17:05:49 +0900

    :art: Add: router provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { appRoutes } from "./routes/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = createBrowserRouter([...appRoutes]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={root} />
+  </React.StrictMode>
+);
