01eba21d71d10d1dc9aa229ee76e43c0eb463cb4
Ruby <rkdfnql22@gmail.com>
2023-10-04 18:49:59 +0900

    :construction: 작업 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "./features/auth/hooks/useAuth";
+import { useUser } from "./features/user/hooks/useUser";
+import { useEffect } from "react";
 
 function App() {
+  const { isLogin, signIn, signOut } = useAuth();
+  const { profile, fetchProfile } = useUser();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    (async () => {
+      await fetchProfile();
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      if (isLogin) {
+        navigate("/board/list");
+        return;
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (profile !== undefined) {
+      signIn();
+    } else {
+      signOut();
+      navigate("/auth/login");
+    }
+  }, [navigate, profile, signIn, signOut]);
+
   return (
     <>
       <Outlet />
