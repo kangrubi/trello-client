393a12d8616f03fac94572312e2d1da37efca0e7
Ruby <rkdfnql22@gmail.com>
2023-10-05 12:11:07 +0900

    :art: Update: interceptors response 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,11 @@ import axios, {
 } from "axios";
 import { API_URL } from "../config";
 import { storage } from "../utils/storage";
-import { CustomError } from "../features/auth/types";
+import {
+  CustomError,
+  PublicApiResponse,
+  RefreshResponse,
+} from "../features/auth/types";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -20,24 +24,30 @@ axiosInstance.interceptors.request.use(
 
     return config;
   },
-  (error: AxiosError) => {
+  (error: AxiosError | Error): Promise<AxiosError> => {
     return Promise.reject(error);
   }
 );
 
-// axiosInstance.interceptors.response.use(
-//   (response: AxiosResponse): AxiosResponse => {
-//     return response;
-//   },
-//   (error: AxiosError) => {
-//     const _error = error as AxiosError<CustomError>;
-//     const { response } = _error;
-//     const originalConfig = _error.config as AxiosRequestConfig;
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse): AxiosResponse => {
+    return response;
+  },
+  async (error: AxiosError | Error): Promise<AxiosError> => {
+    const _error = error as AxiosError<CustomError>;
+    const { response } = _error;
+    const originalConfig = _error.config as AxiosRequestConfig;
 
-//     if (response?.status === 401) {
-//       //
-//     }
+    if (response?.status === 401) {
+      const response = await axios.get<PublicApiResponse<RefreshResponse>>(
+        `${API_URL}/api/v1/auth/refresh`
+      );
 
-//     return Promise.reject(error);
-//   }
-// );
+      storage.setToken(response.data.data.accessToken);
+
+      return axiosInstance(originalConfig);
+    }
+
+    return Promise.reject(error);
+  }
+);
