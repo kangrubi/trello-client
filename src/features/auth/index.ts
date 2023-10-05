d18d91fcd0490548956586b004bc0a913a6d06b9
Ruby <rkdfnql22@gmail.com>
2023-10-05 18:43:36 +0900

    :art: Add: reset password 추가

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -2,5 +2,6 @@ import AuthLayout from "./layout/AuthLayout";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
-export { AuthLayout, Register, Login, ForgotPassword };
+export { AuthLayout, Register, Login, ForgotPassword, ResetPassword };
