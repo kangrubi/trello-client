7e4f4ad1ca127eb4c6a53a490bd646e08c9d17a9
Ruby <rkdfnql22@gmail.com>
2023-10-05 17:25:47 +0900

    :art: Add: ForgotPassword 추가

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,5 +1,6 @@
 import AuthLayout from "./layout/AuthLayout";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 
-export { AuthLayout, Register, Login };
+export { AuthLayout, Register, Login, ForgotPassword };
