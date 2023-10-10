97170b67bce5856cd7213abd2951a4684f287350
Ruby <rkdfnql22@gmail.com>
2023-10-10 15:06:07 +0900

    :art: Add: register 기능 추가

--- src/features/auth/pages/Register.tsx
+++ src/features/auth/pages/Register.tsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { RegisterParams } from "../types";
+import { useAuth } from "../hooks/useAuth";
 
 const Register = () => {
-  return <div></div>;
+  const { register: callRegisterAPI, error } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterParams>();
+
+  const onSubmit: SubmitHandler<RegisterParams> = async (
+    data: RegisterParams
+  ) => {
+    await callRegisterAPI(data);
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert(error.message.message);
+    }
+  }, [error]);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          {...register("username", {
+            required: true,
+            pattern: {
+              value: /^[a-zA-Z0-9]*$/,
+              message: "영어 대소문자와 숫자를 입력해주세요",
+            },
+          })}
+        />
+        {errors.username?.message}
+      </div>
+      <div>
+        <label htmlFor="email">email</label>
+        <input
+          type="text"
+          id="email"
+          placeholder="email"
+          {...register("email", {
+            required: true,
+            pattern: {
+              value:
+                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+              message: "이메일 형식이 맞지않습니다.",
+            },
+          })}
+        />
+        {errors.email?.message}
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          {...register("password", {
+            required: true,
+            pattern: {
+              value:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,128}$/,
+              message:
+                "패스워드 대소문자, 숫자, 특수문자 중 5가지 이상 조합하여 8~128자리 이내를 입력하세요",
+            },
+          })}
+        />
+        {errors.password?.message}
+      </div>
+      <button type="submit">회원가입</button>
+    </form>
+  );
 };
 
 export default Register;
