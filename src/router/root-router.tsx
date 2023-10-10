36db46edd77e7f0d6a01016249968d2bcb632c96
Ruby <rkdfnql22@gmail.com>
2023-10-10 15:07:55 +0900

    :art: Update: auth provider 추가

--- src/router/root-router.tsx
+++ src/router/root-router.tsx
@@ -1,11 +1,18 @@
+import { AuthProvider } from "@/features/auth/providers/AuthProvider";
 import authRoutes from "@/features/auth/routes/auth-routes";
+import { AuthService } from "@/features/auth/service/auth-service";
 import RootLayout from "@/layout/RootLayout";
+import httpService from "@/lib/http";
 import { createBrowserRouter } from "react-router-dom";
 
 const rootRouter = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout />,
+    element: (
+      <AuthProvider authService={new AuthService(httpService)}>
+        <RootLayout />
+      </AuthProvider>
+    ),
     children: [...authRoutes],
   },
 ]);
