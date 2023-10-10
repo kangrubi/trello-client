2ee547a038b4f4eb4c65d7fb89e813a73519be5c
Ruby <rkdfnql22@gmail.com>
2023-10-10 15:06:56 +0900

    :art: Add: auth provider 추가

---
+++ src/features/auth/providers/AuthProvider.tsx
@@ -0,0 +1,53 @@
+import { createContext, useState } from "react";
+import { IAuthService } from "../service/auth-service";
+import { CustomError, RegisterParams } from "../types";
+import { AxiosError } from "axios";
+
+interface AuthContextProps {
+  login(email: string, password: string): Promise<void>;
+  logout(): Promise<void>;
+  register(params: RegisterParams): Promise<void>;
+  error: CustomError | undefined;
+}
+
+export const AuthContext = createContext<AuthContextProps | undefined>(
+  undefined
+);
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+  authService: IAuthService;
+}
+
+export const AuthProvider = ({ children, authService }: AuthProviderProps) => {
+  const [error, setError] = useState<CustomError>();
+
+  const login = async () => {
+    authService.login("hello", "world");
+  };
+  const logout = async () => {
+    authService.logout();
+  };
+  const register = async (request: RegisterParams) => {
+    try {
+      const response = await authService.register(request);
+      return response;
+    } catch (error: unknown) {
+      const _error = error as AxiosError<CustomError>;
+      if (!_error.response) return;
+      setError(_error.response.data);
+    }
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        error,
+        login,
+        logout,
+        register,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
