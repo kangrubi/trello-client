fe22ce16449496f93a85dd5791636a1e8ca53b3f
Ruby <rkdfnql22@gmail.com>
2023-10-10 10:33:06 +0900

    :wrench: Add: path 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
   server: {
     port: 7777,
     https: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
