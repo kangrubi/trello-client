0a4cafdabc9a680a66fc8ebd81f80bfa81d31d9f
Ruby <rkdfnql22@gmail.com>
2023-10-10 10:39:31 +0900

    :art: Add: router provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import rootRouter from "./router/root-router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={rootRouter} />
+  </React.StrictMode>
 );
