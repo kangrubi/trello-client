660cf18fde7807b519650c9bfb440c73a4e773d3
Ruby <rkdfnql22@gmail.com>
2023-10-11 14:08:48 +0900

    :art: Update: error alert ui 수정

--- src/features/auth/pages/Register.tsx
+++ src/features/auth/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { useAuth } from "../hooks/useAuth";
 import { Link, useNavigate } from "react-router-dom";
@@ -11,10 +11,21 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
+import { z } from "zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import registerSchema from "../schema/register-schema";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const Register = () => {
   const { register, error } = useAuth();
@@ -38,78 +49,94 @@ const Register = () => {
     }
   };
 
-  useEffect(() => {
-    if (error) {
-      alert(error.message.message);
-    }
-  }, [error]);
-
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="w-96 rounded-lg border bg-card text-card-foreground shadow-sm p-6">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="username">Username</FormLabel>
-                  <FormControl>
-                    <Input id="username" placeholder="username" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      id="email"
-                      placeholder="email"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      id="password"
-                      placeholder="password"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex items-center justify-center">
-              <Button type="submit" className="w-full">
-                Sign Up
-              </Button>
-            </div>
-            <div className="text-center">
-              <Link to="/auth/login">이미 계정이 있습니까? 로그인</Link>
-            </div>
-          </form>
-        </Form>
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-96 rounded-lg border bg-card text-card-foreground shadow-sm p-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="username">Username</FormLabel>
+                    <FormControl>
+                      <Input id="username" placeholder="username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        id="email"
+                        placeholder="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        id="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center justify-center">
+                <AlertDialog>
+                  <AlertDialogTrigger className="w-full">
+                    <Button type="submit" className="w-full">
+                      Sign Up
+                    </Button>
+                  </AlertDialogTrigger>
+                  {error && (
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          {error.message.error}
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          {error.message.message}
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  )}
+                </AlertDialog>
+              </div>
+              <div className="flex items-center justify-center text-sm">
+                <Link to="/auth/login">이미 계정이 있습니까? 로그인</Link>
+              </div>
+            </form>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
