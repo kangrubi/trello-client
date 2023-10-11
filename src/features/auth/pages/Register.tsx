7d88325dc8732e046c75ec75f948c08bfc499f5b
lee muna <muna@retgames.co.kr>
2023-10-11 10:18:54 +0900

    🔧 fix(Register.tsx): registerSchema를 외부 파일로 분리하여 임포트 📦 feat(register-schema.ts): 회원가입 유효성 검사 스키마를 별도의 파일로 분리하여 임포트

--- src/features/auth/pages/Register.tsx
+++ src/features/auth/pages/Register.tsx
@@ -15,23 +15,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
-const registerSchema = z.object({
-  username: z
-    .string()
-    .min(3, { message: "최소 3글자 이상 입력해주세요" })
-    .regex(
-      /^[a-zA-Z0-9]*$/,
-      "영어 대소문자 또는 대소문자와 숫자를 입력해주세요"
-    ),
-  email: z.string().email({ message: "이메일 형식을 입력해주세요" }),
-  password: z
-    .string()
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,128}$/,
-      "패스워드 대소문자, 숫자, 특수문자 중 5가지 이상 조합하여 8~128자리 이내를 입력하세요"
-    ),
-});
+import registerSchema from "../schema/register-schema";
 
 const Register = () => {
   const { register, error } = useAuth();
@@ -47,9 +31,7 @@ const Register = () => {
     },
   });
 
-  const onSubmit: SubmitHandler<RegisterParams> = async (
-    data: z.infer<typeof registerSchema>
-  ) => {
+  const onSubmit = async (data: z.infer<typeof registerSchema>) => {
     const response = await register(data);
 
     if (response?.statusCode === 201) {
---
+++ src/features/auth/schema/register-schema.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+const registerSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "최소 3글자 이상 입력해주세요" })
+    .regex(
+      /^[a-zA-Z0-9]*$/,
+      "영어 대소문자 또는 대소문자와 숫자를 입력해주세요"
+    ),
+  email: z.string().email({ message: "이메일 형식을 입력해주세요" }),
+  password: z
+    .string()
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,128}$/,
+      "패스워드 대소문자, 숫자, 특수문자 중 5가지 이상 조합하여 8~128자리 이내를 입력하세요"
+    ),
+});
+
+export default registerSchema;
