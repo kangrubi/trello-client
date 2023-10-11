ab77c5d6fcd37f57b1366cd3a90eacc93c273908
RetGamesX <retgamesx@RetGamesXui-MacBookPro.local>
2023-10-11 17:54:09 +0900

    :art: Add:  withCredentials 추가

--- src/features/auth/service/auth-service.ts
+++ src/features/auth/service/auth-service.ts
@@ -21,7 +21,7 @@ export class AuthService implements IAuthService {
   async login(params: LoginParams): Promise<PublicApiResponse<LoginResponse>> {
     const response = await this.httpService.post<
       PublicApiResponse<LoginResponse>
-    >("/api/v1/auth/login", params);
+    >("/api/v1/auth/login", params, { withCredentials: true });
 
     return response;
   }
