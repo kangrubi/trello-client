86f73e2a5143237abc8d24cefe3598e6a7cf10f3
kangrubi <rubi0624@nate.com>
2023-10-12 22:38:20 +0900

    :art: Add: profile api 추가

---
+++ src/features/user/service/user-service.ts
@@ -0,0 +1,19 @@
+import { PublicApiResponse } from "../../auth/types/index";
+import { ProfileResponse } from "../types";
+import { IHttpService } from "@/lib/http";
+
+export interface IUserService {
+  profile(): Promise<PublicApiResponse<ProfileResponse>>;
+}
+
+export class UserService implements IUserService {
+  constructor(private readonly httpService: IHttpService) {}
+
+  async profile(): Promise<PublicApiResponse<ProfileResponse>> {
+    const response = await this.httpService.get<
+      PublicApiResponse<ProfileResponse>
+    >("/api/v1/user/profile");
+
+    return response;
+  }
+}
