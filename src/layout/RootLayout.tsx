30b1ead0c0d208515d8e5f84fcbd0afda4a68539
kangrubi <rubi0624@nate.com>
2023-10-12 22:55:05 +0900

    :construction: 작업 중

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,9 +1,32 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { useAuth } from "@/features/auth/hooks/useAuth";
+import { useUser } from "@/features/user/hooks/useUser";
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const RootLayout = () => {
+  const { profile, userProfile } = useUser();
+  const { isLogin, signIn, signOut } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await profile();
+        signIn();
+      } catch (error) {
+        if (location.pathname.includes("/auth")) return;
+        navigate("/auth/login");
+        signOut();
+      }
+    })();
+  }, []);
+
   return (
     <div>
+      <h2>gg</h2>
+      {userProfile?.username}
       <Outlet />
     </div>
   );
