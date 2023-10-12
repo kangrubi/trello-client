506a9a8c06ef0eba29bdf38e437ab4a58fbd0c68
kangrubi <rubi0624@nate.com>
2023-10-12 22:37:24 +0900

    :label: Add: profile type 추가

---
+++ src/features/user/types/index.ts
@@ -0,0 +1,11 @@
+export interface ProfileResponse {
+  username: string;
+  email: string;
+  profileImage?: string;
+}
+
+export interface UserProfile {
+  username: string;
+  email: string;
+  profileImage?: string;
+}
