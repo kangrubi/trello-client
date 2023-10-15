7d5f3eaf250ff372af35a54d64e0451072b84f48
kangrubi <rubi0624@nate.com>
2023-10-15 22:53:37 +0900

    :art: Add: ForgotPassword 추가

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,4 +1,5 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 
-export { Register, Login };
+export { Register, Login, ForgotPassword };
